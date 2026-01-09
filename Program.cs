16d76db490b0f26ec42363cedd4b24bc0620626d
StuntJ <dlwornr0919@gmail.com>
2026-01-09 11:56:05 +0900

    feat(player): 직업선택 로직

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using TextRPG.Utils;
+using TextRPG.Data;
 
 namespace BssenTextRPG
 {
@@ -8,12 +9,14 @@ namespace BssenTextRPG
         {
             //콘솔 인코딩 설정 (한글 지원)
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            
+
             //타이틀 표시
-            ConsoleUI.ShowTitle();
+            GameManager.Instance.StartGame();
 
             //TODO: 저장된 게임 존재 여부 확인
             //TODO: 게임 로드 및 새 게임 시작
+
+
 
         }
     }
