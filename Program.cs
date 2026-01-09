223e6ae8d265eb4135b4cb9bd2ed15b2111e6a0c
StuntJ <dlwornr0919@gmail.com>
2026-01-09 11:10:49 +0900

    feat(project): 타이틀 화면 표시

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace BssenTextRPG
+﻿using TextRPG.Utils;
+
+namespace BssenTextRPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //콘솔 인코딩 설정 (한글 지원)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            //타이틀 표시
+            ConsoleUI.ShowTitle();
+
+            //TODO: 저장된 게임 존재 여부 확인
+            //TODO: 게임 로드 및 새 게임 시작
+
         }
     }
 }
