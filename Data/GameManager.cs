61280e91bbd48436eadb96f7a94025a9ffbbd657
StuntJ <dlwornr0919@gmail.com>
2026-01-11 14:49:10 +0900

    feat(player): Player 캐릭터 생성

--- Data/GameManager.cs
+++ Data/GameManager.cs
@@ -30,6 +30,11 @@ class GameManager
     }
 
     #endregion
+
+    #region 프로퍼티
+    public Player? player { get; private set; }
+    #endregion
+
     #region 게임 시작 / 종료
     //게임 시작 메서드
     public void StartGame()
@@ -85,13 +90,13 @@ class GameManager
                     Console.WriteLine("잘못된 입력입니다. 다시 선택해주세요.");
                     continue;
             }
+
             break;
         }
-        
+        //입력한 이름과 선택한 직업으로 플레이어 캐릭터 생성
+        player = new Player(name, job);
+        Console.WriteLine($"\n{name}님, {job}직업으로 캐릭터가 생성되었습니다.");
     }
-
-    //0: 전사, 1: 마법사, 2: 궁수 - 열거형 변수를 선언
-    
     #endregion
 }
 
